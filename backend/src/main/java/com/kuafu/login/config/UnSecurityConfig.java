04d218f4812a6fd74a215382b18e09da8e25626c
Yake <jiangfei@kuafuai.net>
2024-06-03 21:46:39 +0800

    feat(chat): support chatbot

--- backend/src/main/java/com/kuafu/login/config/UnSecurityConfig.java
+++ backend/src/main/java/com/kuafu/login/config/UnSecurityConfig.java
@@ -2,10 +2,13 @@ package com.kuafu.login.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 
 @Configuration
@@ -25,5 +28,12 @@ public class UnSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .headers().frameOptions().disable();
+    }
+
+    @Bean
+    public HttpFirewall allowDoubleSlashFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowUrlEncodedDoubleSlash(true);
+        return firewall;
     }
 }
