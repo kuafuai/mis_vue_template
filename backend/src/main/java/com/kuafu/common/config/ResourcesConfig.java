6eadcfe7de16bc2c7db6e9407ded3509431e88fd
Yake <jiangfei@kuafuai.net>
2024-06-03 21:20:42 +0800

    feat(chat): support chatbot

--- backend/src/main/java/com/kuafu/common/config/ResourcesConfig.java
+++ backend/src/main/java/com/kuafu/common/config/ResourcesConfig.java
@@ -1,10 +1,7 @@
 package com.kuafu.common.config;
 
 import com.kuafu.common.constant.Constants;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.firewall.HttpFirewall;
-import org.springframework.security.web.firewall.StrictHttpFirewall;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,12 +15,5 @@ public class ResourcesConfig implements WebMvcConfigurer {
         registry.addResourceHandler(Constants.RESOURCE_PREFIX + "/**")
                 .addResourceLocations("file:" + AppConfig.getProfile() + "/");
 
-    }
-
-    @Bean
-    public HttpFirewall allowDoubleSlashFirewall() {
-        StrictHttpFirewall firewall = new StrictHttpFirewall();
-        firewall.setAllowUrlEncodedDoubleSlash(true);
-        return firewall;
     }
 }
