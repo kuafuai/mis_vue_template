9a3bdabfc5e53c64c0f1144658bfd32059375227
Yake <jiangfei@kuafuai.net>
2024-07-03 20:20:29 +0800

    feat(crud):search from

--- frontend/src/components/Form/SearchForm.vue
+++ frontend/src/components/Form/SearchForm.vue
@@ -1,5 +1,6 @@
 <template>
-  <el-form :inline="true" :model="props.searchData" class="search-form-inline">
+  <base-layout display="flex" x="between" y="end">
+  <el-form :inline="true" :model="props.searchData" class="search-form-inline w-full">
     <slot />
     
     <el-form-item
@@ -14,10 +15,26 @@
       />
     </el-form-item>
 
-    <el-form-item>
-      <el-button @click="onSearch" type="primary">搜索</el-button>
-    </el-form-item>
+    <el-collapse-transition>
+      <div v-show="advanced">
+        <slot name="collapse">
+        </slot>
+      </div>
+    </el-collapse-transition>
   </el-form>
+  
+    <div style="width: 180px; margin-bottom: 18px;">
+        <div style="float: right;">
+          <el-button @click="onSearch" type="primary">搜索</el-button>
+          <a style="margin-left: 8px" @click="toggleAdvanced">
+            <!-- {{ advanced ? '收起' : '展开' }} -->
+            <el-icon v-if="advanced"><ArrowUp /></el-icon>
+            <el-icon v-else><ArrowDown /></el-icon>
+          </a>
+        </div>
+      </div>
+  </base-layout>    
+  
 </template>
 
 <script setup>
@@ -27,11 +44,17 @@ const props = defineProps({
   searchData: { type: Object, default: () => {} },
 });
 
+const advanced = ref(false);
+
 // 定义事件
 const emit = defineEmits(["refreshTableData"]);
 
 const onSearch = () => {
   emit("refreshTableData");
 };
+
+function toggleAdvanced() {
+  advanced.value = !advanced.value
+}
 
 </script>
