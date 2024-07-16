16fe2aec71e47e6930b0640f56786a3f144590d8
Yake <jiangfei@kuafuai.net>
2024-07-16 21:38:39 +0800

    feat(UI):new UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <pc v-if="type==='PC'" />
+  <pc_new v-if="type==='PC'" />
   <phone v-else />
   
 </template>
 
 <script setup>
 import pc from '@/layout/pc.vue'
+import pc_new from '@/layout/pc_new.vue'
 import phone from '@/layout/phone.vue'
 
 const type = import.meta.env.VITE_APP_TYPE;
