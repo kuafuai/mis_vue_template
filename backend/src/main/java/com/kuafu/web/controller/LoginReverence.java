4f05c21e906187a3501cbc9e8d5202fdff4c91f6
kuafu-limanxiang <limanxiang@kuafuai.net>
2024-09-12 10:27:58 +0800

    feat/login_manger: 删除生成的信息

--- backend/src/main/java/com/kuafu/web/controller/LoginReverence.java
+++ backend/src/main/java/com/kuafu/web/controller/LoginReverence.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 @Getter
 public enum LoginReverence {
 
-industry("行业表","industry"),personnelScale("人员规模","personnelScale"),customerHierarchy("客户分层","customerHierarchy"),leadStatus("线索状态","leadStatus"),customerStage("客户阶段","customerStage"),opportunityStatus("商机状态","opportunityStatus"),salesman("销售人员","salesman"),customer("客户表","customer"),visitManagement("客户拜访","visitManagement"),businessOpportunity("商机管理","businessOpportunity"),login("登录表","login"),;
+    None("none", "none");
     private String label;
     private String value;
 
